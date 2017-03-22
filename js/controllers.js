beef0296ffecd3846b2b229870ce4d481e9ad2fb
Luan Andrade <luansandrade92@hotmail.com>
2017-03-22 11:09:58 -0300

    angularjs guide controllers

--- js/controllers.js
+++ js/controllers.js
@@ -1,12 +1,30 @@
 app
 
 .controller('InvoiceController',
-    function(currencyConverter){
+    function(currencyConverter, $scope){
     this.qty =1;
     this.cost = 2;
     this.inCurr = 'USD';
     this.currencies = currencyConverter.currencies;
     this.message = "hello";
+    $scope.bla = "bla bla bla";
+    $scope.spice = "very";
+
+    $scope.double = function(n){
+        return 2 * n;
+    };
+
+    $scope.chiliSpicy = function(){
+        $scope.spice = 'chili';
+    };
+
+    $scope.jalapenoSpicy = function(){
+        $scope.spice = 'jalapeno';
+    };
+
+    $scope.customSpicy= function(spice){
+        $scope.spice = spice;
+    };
 
     this.usdToForeignRates = {
         USD: 1,
@@ -21,4 +39,19 @@ app
     this.pay = function(){
         alert('thanks');
     };
-});
+})
+
+.controller('MainController', ['$scope', function($scope) {
+  $scope.timeOfDay = 'morning';
+  $scope.name = 'Nikki';
+}])
+.controller('ChildController', ['$scope', function($scope) {
+  $scope.name = 'Mattie';
+}])
+
+.controller('GrandChildController', ['$scope', function($scope) {
+  $scope.timeOfDay = 'evening';
+  $scope.name = 'Gingerbread Baby';
+}])
+
+;
