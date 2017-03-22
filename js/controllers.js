2c906c36a2a3ce7db87a560a0bdecb901315b21d
Luan Andrade <luansandrade92@hotmail.com>
2017-03-22 15:50:21 -0300

    Simple form

--- js/controllers.js
+++ js/controllers.js
@@ -108,4 +108,18 @@ app
     };
 })
 
+.controller('FormController', function($scope){
+    $scope.master = {};
+
+    $scope.update = function(user){
+        $scope.master = angular.copy(user);
+    };
+
+    $scope.reset = function(){
+        $scope.user = angular.copy($scope.master);
+    };
+
+    $scope.reset();
+})
+
 ;
