ad4ef5acfe865607c016695f123e3ce6e3fffabb
Luan Andrade <luansandrade92@hotmail.com>
2017-03-22 16:55:15 -0300

    method finally()

--- js/app.js
+++ js/app.js
@@ -4,18 +4,15 @@ var INTEGER_REGEXP = /^-?\d+$/;
 
 function getData($timeout, $q) {
   return function() {
-    var defer = $q.defer();
-
-    // simulated async function
-    $timeout(function() {
-        if(Math.round(Math.random())) {
-            defer.resolve('data received!')
-        } else {
-            defer.reject('oh no and error! try again')
-        }
-    }, 2000);
-
-    return defer.promise
+    return $q(function(resolve,reject){
+        $timeout(function(){
+            if(Math.round(Math.random())){
+                resolve('data received!')
+            } else {
+                reject('oh no an error! try again')
+            }
+        }, 2000)
+    })
   }
 }
 
@@ -26,5 +23,8 @@ app.factory('getData', getData)
           console.log(string)
       }, function(error){
           console.error(error)
+      })
+      .finally(function(){
+          console.log('finished at:', new Date())
       })
 })
