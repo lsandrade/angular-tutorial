fb30c36cb807554a087a1b979ab2369fc0af0e29
Luan Andrade <luansandrade92@hotmail.com>
2017-03-22 16:28:38 -0300

    custom form controls (using ngModel)

--- js/directives.js
+++ js/directives.js
@@ -71,4 +71,24 @@ app
   };
 })
 
+.directive('contenteditable', function() {
+  return {
+    require: 'ngModel',
+    link: function(scope, elm, attrs, ctrl) {
+      // view -> model
+      elm.on('blur', function() {
+        ctrl.$setViewValue(elm.html());
+      });
+
+      // model -> view
+      ctrl.$render = function() {
+        elm.html(ctrl.$viewValue);
+      };
+
+      // load init value from DOM
+      ctrl.$setViewValue(elm.html());
+    }
+  };
+})
+
 ;
