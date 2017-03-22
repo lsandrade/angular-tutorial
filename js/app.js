240fdb9e33659eadc085c9fca1268cee1c614ca6
Luan Andrade <luansandrade92@hotmail.com>
2017-03-22 16:48:22 -0300

    promises accept and reject

--- js/app.js
+++ js/app.js
@@ -1,3 +1,30 @@
 var app = angular.module('invoice1', []);
 
 var INTEGER_REGEXP = /^-?\d+$/;
+
+function getData($timeout, $q) {
+  return function() {
+    var defer = $q.defer();
+
+    // simulated async function
+    $timeout(function() {
+        if(Math.round(Math.random())) {
+            defer.resolve('data received!')
+        } else {
+            defer.reject('oh no and error! try again')
+        }
+    }, 2000);
+
+    return defer.promise
+  }
+}
+
+app.factory('getData', getData)
+.run(function(getData) {
+  var promise = getData()
+      .then(function(string){
+          console.log(string)
+      }, function(error){
+          console.error(error)
+      })
+})
