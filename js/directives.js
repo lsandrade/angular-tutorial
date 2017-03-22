8c0948086063d46618e6abdf0f11706e0ad6b7fa
Luan Andrade <luansandrade92@hotmail.com>
2017-03-22 16:25:04 -0300

    Modifying built-in validators (e-mail)

--- js/directives.js
+++ js/directives.js
@@ -52,4 +52,23 @@ app
     }
   };
 })
+
+.directive('overwriteEmail', function() {
+  var EMAIL_REGEXP = /^[a-z0-9!#$%&'*+/=?^_`{|}~.-]+@example\.com$/i;
+
+  return {
+    require: '?ngModel',
+    link: function(scope, elm, attrs, ctrl) {
+      // only apply the validator if ngModel is present and AngularJS has added the email validator
+      if (ctrl && ctrl.$validators.email) {
+
+        // this will overwrite the default AngularJS email validator
+        ctrl.$validators.email = function(modelValue) {
+          return ctrl.$isEmpty(modelValue) || EMAIL_REGEXP.test(modelValue);
+        };
+      }
+    }
+  };
+})
+
 ;
