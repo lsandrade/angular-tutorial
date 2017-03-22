f18b2c08d065809dd685d8df74a08d8f1d11d625
Luan Andrade <luansandrade92@hotmail.com>
2017-03-22 13:19:07 -0300

    expressions

---
+++ js/protactor.js
@@ -0,0 +1,9 @@
+
+it('should allow user expression testing', function(){
+    element(by.css('.expressions button')).click();
+    var lis = element(by.css('.expressions ul'))
+        .all(by.repeater('expr in exprs'));
+    expect(list.count()).toBe(1);
+    expect(lis.get(0).getText())
+        .toEqual('[ X ] 3*10|currency => $30.00');
+});
