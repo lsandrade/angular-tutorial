2955ffa2faf381338203098b6971ef93bbc8de3b
Luan Andrade <luansandrade92@hotmail.com>
2017-03-23 12:03:07 -0300

    Directive Communication

--- js/directives.js
+++ js/directives.js
@@ -221,5 +221,36 @@ app
   };
 })
 
-;
+.directive("country", function(){
+  return {
+    restrict: "E",
+      controller: function(){
+        this.makeAnnouncement = function(message){
+          console.log("Country says: " + message);
+        }
+      }
+  }
+})
+
+.directive("state", function(){
+  return {
+    restrict: "E",
+      controller: function(){
+      this.makeLaw = function(law){
+        console.log("Law: "+law)
+      };
+      }
+  };
+})
+
+.directive("city", function(){
+  return {
+    restrict: "E",
+      require: ["^country", "^state"],
+      link: function(scope, element, attrs, ctrls){
+        ctrls[0].makeAnnouncement("This City rocks");
+        ctrls[1].makeLaw("Jump higher");
+      }
+  };
+})
 ;
