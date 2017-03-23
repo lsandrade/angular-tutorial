f70244cd3e682fb26b3f85fe2d2d025564433f1b
Luan Andrade <luansandrade92@hotmail.com>
2017-03-23 11:55:12 -0300

    Components and Containers

--- js/directives.js
+++ js/directives.js
@@ -198,5 +198,28 @@ app
   }
 })
 
+.directive('clock', function(){
+  return {
+    restrict: 'E',
+      scope: {
+        timezone: "@"
+      },
+      template: "<div>12:00pm {{timezone}}</div>"
+  };
+})
 
+.directive("panel", function(){
+  return {
+    restrict: "E",
+      transclude: true,
+      scope: {
+        title: "@"
+      },
+      template: "<div style='border: 3px solid #000000'>"+
+          "<div class='alert-box'>{{title}}</div>"+
+          "<div ng-transclude></div></div>"
+  };
+})
+
+;
 ;
