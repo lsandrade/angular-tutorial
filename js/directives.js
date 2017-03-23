808a3349edf8b893450ef14218335a59cd46ae65
Luan Andrade <luansandrade92@hotmail.com>
2017-03-23 10:59:53 -0300

    Communicating Between Directives

--- js/directives.js
+++ js/directives.js
@@ -156,8 +156,25 @@ app
     restrict: 'E',
     transclude: true,
     template: '<h2>Hello world!</h2> <div role="tabpanel" ng-transclude></div>',
-    scope: { },
+    require: '^tabset',
+      scope: { },
     link: function(scope, elem, attr) {}
+  }
+})
+
+
+.directive('tabset', function() {
+  return {
+    restrict: 'E',
+    transclude: true,
+    scope: { },
+    templateUrl: 'tabset.html',
+    bindToController: true,
+    controllerAs: 'tabset',
+    controller: function() {
+      var self = this
+      self.tabs = []
+    }
   }
 })
 
